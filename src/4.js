24dbf0fe25e9d4dbfc6b6f0633d74c1766c25db6
mamory9382 <ma_mory@gmx.com>
2025-03-07 21:23:26 +0000

    Add 4.js

---
+++ src/4.js
@@ -0,0 +1,28 @@
+// Random JavaScript Code
+
+const randomNumber = Math.floor(Math.random() * 10);
+let randomElement;
+
+if (randomNumber === 0) {
+  randomElement = "A";
+} else if (randomNumber === 1) {
+  randomElement = "B";
+} else if (randomNumber === 2) {
+  randomElement = "C";
+} else if (randomNumber === 3) {
+  randomElement = "D";
+} else if (randomNumber === 4) {
+  randomElement = "E";
+} else if (randomNumber === 5) {
+  randomElement = "F";
+} else if (randomNumber === 6) {
+  randomElement = "G";
+} else if (randomNumber === 7) {
+  randomElement = "H";
+} else if (randomNumber === 8) {
+  randomElement = "I";
+} else if (randomNumber === 9) {
+  randomElement = "J";
+}
+
+console.log(randomElement);
